166c4685f3d73edd7a48d075852bb4786dd2ada2
Emanuele <cannone.emanuele@gmail.com>
2021-01-07 17:44:59 +0100

    error

--- js/main.js
+++
@@ -1,4 +0,0 @@
-// Hamburger menu:
-// mostrare / nascondere il menu principale
-// (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
-
--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 // Hamburger menu:
 // mostrare / nascondere il menu principale
 // (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+
+$(document).ready(function(){
+
+  $('.header-right a i').click(function (){
+    $('.hamburger-menu').show();
+  });
+
+
+  $(".header-right a i").click(function(){
+    alert("The paragraph was clicked.");
+  });
+
+  $('.header-right a i').click(function (){
+    $('.hamburger-menu').hide();
+  });
+
+});
